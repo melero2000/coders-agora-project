073d96575bb23a6cf1894219139c1c88c4611a73
melero2000 <juanmelerocollado@gmail.com>
2021-01-22 20:00:12 +0100

    Primeros pasos - Wrapper

--- pages/index.js
+++ pages/index.js
@@ -1,65 +1,29 @@
 import Head from 'next/head'
-import styles from '../styles/Home.module.css'
+import { Wrapper } from '../layout/Wrapper'
 
 export default function Home() {
   return (
-    <div className={styles.container}>
-      <Head>
-        <title>Create Next App</title>
-        <link rel="icon" href="/favicon.ico" />
-      </Head>
+    <>
 
-      <main className={styles.main}>
-        <h1 className={styles.title}>
-          Welcome to <a href="https://nextjs.org">Next.js!</a>
-        </h1>
+    <Wrapper>
+      <section>
+          <h1>Coders Agora</h1>
+      </section>
+    </Wrapper>
+    <style jsx>{`
+      h1 {
+        font-size: 5rem;
+      }
 
-        <p className={styles.description}>
-          Get started by editing{' '}
-          <code className={styles.code}>pages/index.js</code>
-        </p>
+      .wrapper {
+        display: grid;
+        grid-template-colums: 1fr min(1200px, 180%) 1fr;
+      }
 
-        <div className={styles.grid}>
-          <a href="https://nextjs.org/docs" className={styles.card}>
-            <h3>Documentation &rarr;</h3>
-            <p>Find in-depth information about Next.js features and API.</p>
-          </a>
-
-          <a href="https://nextjs.org/learn" className={styles.card}>
-            <h3>Learn &rarr;</h3>
-            <p>Learn about Next.js in an interactive course with quizzes!</p>
-          </a>
-
-          <a
-            href="https://github.com/vercel/next.js/tree/master/examples"
-            className={styles.card}
-          >
-            <h3>Examples &rarr;</h3>
-            <p>Discover and deploy boilerplate example Next.js projects.</p>
-          </a>
-
-          <a
-            href="https://vercel.com/import?filter=next.js&utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className={styles.card}
-          >
-            <h3>Deploy &rarr;</h3>
-            <p>
-              Instantly deploy your Next.js site to a public URL with Vercel.
-            </p>
-          </a>
-        </div>
-      </main>
-
-      <footer className={styles.footer}>
-        <a
-          href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Powered by{' '}
-          <img src="/vercel.svg" alt="Vercel Logo" className={styles.logo} />
-        </a>
-      </footer>
-    </div>
+      .content {
+        grid-column-start: 2;
+      }
+    `}</style>
+    </>
   )
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,12 @@ module.exports = {
   purge: ['./pages/**/*.js', './components/**/*.js'],
   darkMode: false, // or 'media' or 'class'
   theme: {
-    extend: {},
+    extend: {
+      gridTemplateColumns: {
+        // Complex site-secific column configuration
+        wrapper: '1fr min(1200px, 100%) 1fr'
+      }
+    },
   },
   variants: {
     extend: {},
